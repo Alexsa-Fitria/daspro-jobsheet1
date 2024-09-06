88f23fd1bfa5e3b4ad9408ed0b18b57627b566ee
Alexsa-Fitria <lexafitriayu@gmail.com>
2024-09-06 09:14:33 +0700

    jobsheet1

---
+++ Latihan02.java
@@ -0,0 +1,19 @@
+/**
+ * Latihan02
+ */
+public class Latihan02 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.err.println("-- Data Mahasiswa Jurusan Teknologi Informmasi POLINEMA--");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama        : Alexsa Fitria Ayu Siswoyo");
+        System.out.println("NIM         : 244107020020");
+        System.out.println("Kelas       : 1E");
+        System.out.println("No Absen    : 02");
+        System.out.println("Prodi       : Teknik Informatika");
+        System.out.println("Alamat      : Jl. Dahlia, Kedung Banteng, Sukorejo, Ponorogo, Jawa Timur");
+        System.out.println("No HP       : 085645016580");
+        System.out.println("Hobi        : Memasak dan Membaca");
+    }
+}
